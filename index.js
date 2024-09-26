91ff9717da43b1478320fc305b594110b9e1a176
afiq1727 <afiqmuhammad791@gmail.com>
2024-09-26 13:29:03 +0700

    seccond commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const adhan = require('adhan');
+
+const coordinates = new adhan.Coordinates(35.7897507, -78.6912485);
+const params = adhan.CalculationMethod.MoonsightingCommittee();
+const date = new Date();
+
+const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+
+console.log(prayerTimes);
